4b898d23bba795081f9419b059b2062a4a7e4c34
Pulipuli Chen <blog@pulipuli.info>
2019-09-30 22:49:34 +0800

    0930 object方案也是不行，還原吧

--- table-thumbnail-rwd.js
+++ table-thumbnail-rwd.js
@@ -67,14 +67,9 @@ let TableThumbnailRWD = function (table, options) {
     table.style.minWidth = CONFIG.thumbnailMinWidth
     domtoimage.toSvg(table)
             .then(function (dataUrl) {
-              /*
-              var img = new Image();
+              let img = new Image();
               img.src = dataUrl;
               let $img = $(img)
-              */
-              let svg = dataUrl.slice(dataUrl.indexOf('<svg'))
-              let $img = $(`<object type="image/svg+xml" data="${svg}"></object>`)
-              
               $img.addClass('table-thumbnail')
                       .addClass(tableClassname)
                       .attr('title', CONFIG.thumbnailTitle)
