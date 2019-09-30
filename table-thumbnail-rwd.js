154a0997afa9e784676d3af653bc38c020ddd47b
Pulipuli Chen <blog@pulipuli.info>
2019-09-30 22:43:46 +0800

    0930 好像還是只能還原...

--- table-thumbnail-rwd.js
+++ table-thumbnail-rwd.js
@@ -3,41 +3,41 @@
 let readyWidth = []
 
 let TableThumbnailRWD = function (table, options) {
-  if (typeof($) !== 'function') {
+  if (typeof ($) !== 'function') {
     throw 'jQuery is requirement. Please install from https://code.jquery.com'
   }
-  if (typeof(domtoimage) !== 'object') {
+  if (typeof (domtoimage) !== 'object') {
     throw 'dom-to-image is requirement. Please install from https://github.com/tsayen/dom-to-image'
   }
-  
-  if (typeof(table.prop) === 'function') {
+
+  if (typeof (table.prop) === 'function') {
     table = table[0]
   }
-  
+
   let CONFIG = {
     showThumbnailWidthPX: 640,
     thumbnailMinWidth: '800px',
     thumbnailTitle: 'Open table in new window',
     popupCssURL: null
   }
-  
-  if (typeof(options) === 'object') {
+
+  if (typeof (options) === 'object') {
     for (let key in CONFIG) {
-      if (typeof(options[key]) !== 'undefined') {
+      if (typeof (options[key]) !== 'undefined') {
         CONFIG[key] = options[key]
       }
     }
   }
-  
+
   let tableClassname = `table-thumbnail-rwd-${CONFIG.showThumbnailWidthPX}`
-  
+
   // ---------------------------------------------
-  
+
   let insertCSSRule = () => {
     if (readyWidth.indexOf(tableClassname) > -1) {
       return false
     }
-    
+
     let css = `.table-thumbnail.${tableClassname} {
   display: none;
   
@@ -55,36 +55,36 @@ let TableThumbnailRWD = function (table, options) {
     display: block;
   }
 }`
-      // https://dev.to/karataev/set-css-styles-with-javascript-3nl5
-      let style = document.createElement('style');
-      style.innerHTML = css
-      document.head.appendChild(style);
-      
-      readyWidth.push(tableClassname)
+    // https://dev.to/karataev/set-css-styles-with-javascript-3nl5
+    let style = document.createElement('style');
+    style.innerHTML = css
+    document.head.appendChild(style);
+
+    readyWidth.push(tableClassname)
   }
 
   let setupTableThumbnail = (table) => {
     table.style.minWidth = CONFIG.thumbnailMinWidth
     domtoimage.toSvg(table)
-      .then(function (dataUrl) {
-          var img = new Image();
-          img.src = dataUrl;
-          let $img = $(img)
-          $img.addClass('table-thumbnail')
-                  .addClass(tableClassname)
-                  .attr('title', CONFIG.thumbnailTitle)
-                  .insertAfter(table)
-          $img.click(function () {
-            let $table = $(this).prev()
-            popupTable($table)
-          })
-          $(table).addClass('table-thumbnail-ready')
-                  .addClass(tableClassname)
-          table.style.minWidth = undefined
-      })
-      .catch(function (error) {
-          console.error('oops, something went wrong!', error);
-      });
+            .then(function (dataUrl) {
+              var img = new Image();
+              img.src = dataUrl;
+              let $img = $(img)
+              $img.addClass('table-thumbnail')
+                      .addClass(tableClassname)
+                      .attr('title', CONFIG.thumbnailTitle)
+                      .insertAfter(table)
+              $img.click(function () {
+                let $table = $(this).prev()
+                popupTable($table)
+              })
+              $(table).addClass('table-thumbnail-ready')
+                      .addClass(tableClassname)
+              table.style.minWidth = undefined
+            })
+            .catch(function (error) {
+              console.error('oops, something went wrong!', error);
+            });
   }
 
   let popupTable = ($table) => {
@@ -92,23 +92,23 @@ let TableThumbnailRWD = function (table, options) {
     win.document.title = 'Table: ' + document.title
 
     let html = $table.prop('outerHTML')
-    if (typeof(CONFIG.popupCssURL) === 'string') {
+    if (typeof (CONFIG.popupCssURL) === 'string') {
       html = html + `<link href='${CONFIG.popupCssURL}' rel='stylesheet' type='text/css'/>`
     }
 
     win.document.body.innerHTML = html
   }
-  
+
   insertCSSRule()
   setupTableThumbnail(table)
 }
 
-if (typeof(window) === 'object') {
+if (typeof (window) === 'object') {
   window.TableThumbnailRWD = TableThumbnailRWD
 }
-if (typeof(exports) !== 'undefined') {
+if (typeof (exports) !== 'undefined') {
   exports.default = TableThumbnailRWD
 }
-if (typeof(module) !== 'undefined') {
+if (typeof (module) !== 'undefined') {
   module.exports = TableThumbnailRWD
 }
